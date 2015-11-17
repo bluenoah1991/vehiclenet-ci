bf28e7710ff1d56fb1677465ab9a24268025124a
Codemeow <codemeow@yahoo.com>
2015-11-17 17:52:48 +0800

    Q:	What's the difference between Bell Labs and the Boy Scouts of America? A:	The Boy Scouts have adult supervision.

--- monitor/install.sh
+++ monitor/install.sh
@@ -2,15 +2,14 @@
 
 apt-get install apache2-utils -y
 #htdigest -c admin.htdigest localhost admin
-htpasswd -c admin.htpasswd admin
+htpasswd -c /admin.htpasswd admin
 docker run \
 	--volume=/:/rootfs:ro \
 	--volume=/var/run:/var/run:rw \
 	--volume=/sys:/sys:ro \
 	--volume=/var/lib/docker/:/var/lib/docker:ro \
-	--volume=$(pwd):/root/monitor:r \
 	--publish=8080:8080 \
 	--detach=true \
 	--name=cadvisor \
 	google/cadvisor:latest \
-	-http_auth_file /root/monitor/admin.htpasswd --http_auth_realm localhost
+	-http_auth_file /rootfs/admin.htpasswd --http_auth_realm localhost
